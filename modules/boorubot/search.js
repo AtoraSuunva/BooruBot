3bb9e99ad3306ada575030ff69816d9486d8e15e
AtlasTheBot <AtlasTheBot@users.noreply.github.com>
2017-02-07 00:28:46 -0500

    Fixed error in dms

--- modules/boorubot/search.js
+++ modules/boorubot/search.js
@@ -13,7 +13,7 @@ module.exports.config = {
 module.exports.events = {}
 module.exports.events.message = (bot, message) => {
   //Horrible idea, I know, but I'm not rewriting all this code just for color
-  let settingsId = (message.guild !== null) ? message.guild.id : message.channel.id //DMs are a channel, interestingly enough
+  let settingsId = (message.guild) ? message.guild.id : message.channel.id //DMs are a channel, interestingly enough
   let settings = bot.modules.settings.get(settingsId)
   let args = bot.modules.shlex(message.content)
 
@@ -37,7 +37,7 @@ module.exports.events.message = (bot, message) => {
   // ['e6', 'cat', 'cute']
 
   if (!module.exports.config.invokers.filter(_=>_!=='').includes(args[0])) {
-    if (booru.resolveSite(args[0]) || ['rand', 'random'].includes(args[0])) {
+    if (booru.resolveSite(args[0]) || ['rand', 'random', 'r'].includes(args[0])) {
       args.unshift('')
     } else {
       return
@@ -122,7 +122,7 @@ function randSearch(tags, message) {
 
   	let imgs
     let randSites = Object.keys(booru.sites).sort(_=>Math.random()-0.5)
-    let settingsId = (message.guild !== null) ? message.guild.id : message.channel.id //DMs are a channel, interestingly enough
+    let settingsId = (message.guild) ? message.guild.id : message.channel.id //DMs are a channel, interestingly enough
     let settings = message.botClient.modules.settings.get(settingsId) //yay settings
     let searchStart = process.hrtime()
 
@@ -160,7 +160,7 @@ function compareArrays(arr1, arr2) {
 //Format the embed so I don't have to copy paste
 function postEmbed(img, siteUrl, searchTime, message) {
 
-  if (message.guild !== null && !message.channel.permissionsFor(message.client.user).hasPermission('EMBED_LINKS')) {
+  if (message.guild && !message.channel.permissionsFor(message.client.user).hasPermission('EMBED_LINKS')) {
     return message.channel.send(
     encodeURI(`https://${siteUrl}${booru.sites[siteUrl].postView}${img.common.id}`) + '\n' +
     encodeURI(img.common.file_url) + '\nBooruBot works better if it has the "embeds links" permission'
@@ -169,8 +169,7 @@ function postEmbed(img, siteUrl, searchTime, message) {
   }
 
   let metadata = {
-    user: message.author.id,
-
+    user: message.author.id
   }
 
   let Discord = require('discord.js')
@@ -182,31 +181,31 @@ function postEmbed(img, siteUrl, searchTime, message) {
     image: {url: encodeURI(img.common.file_url)},
     url: encodeURI(img.common.file_url),
     footer: {
-      text: `${siteUrl} - ${(searchTime[0] * 1e9 + searchTime[1])/1000000}ms`,
+      text: `${siteUrl} - ${((searchTime[0] * 1e9 + searchTime[1])/1000000).toFixed(3)}ms`,
       icon_url: `https://${siteUrl}/favicon.ico` //pray they have their favicon here like a regular site
     }
   })
 
   embed.setDescription(`**Score:** ${img.common.score} | **Rating:** ${img.common.rating.toUpperCase()} | [Image](${encodeURI(img.common.file_url)}) [](${JSON.stringify(metadata)})`)
 
-  embed.setColor((message.guild !== null) ? message.guild.members.get(message.client.user.id).highestRole.color : '#34363C')
+  embed.setColor((message.guild) ? message.guild.members.get(message.client.user.id).highestRole.color : '#34363C')
 
   //console.log(require('util').inspect(embed, { depth: null }));
 
   message.channel.send({embed})
-  .then(msg => {
-    message.channel.lastImagePosted = msg //Lazy way to easily delete the last image posted, see `delete.js`
+    .then(msg => {
+      message.channel.lastImagePosted = msg //Lazy way to easily delete the last image posted, see `delete.js`
 
-    let customEmote = new Map([['264246678347972610', '272782646407593986'],['211956704798048256', '269682750682955777']]).get(message.guild.id)
-    //NSFW, squares
-    //test server, glaceWhoa
+      let customEmote = (message.guild) ? new Map([['264246678347972610', '272782646407593986'],['211956704798048256', '269682750682955777']]).get(message.guild.id) : null
+      //NSFW, squares
+      //test server, glaceWhoa
 
-    if (customEmote)
-      msg.react(message.client.emojis.get(customEmote))
-    else
-      msg.react('❌')
+      if (customEmote)
+        msg.react(message.client.emojis.get(customEmote)).catch() //presumably we can't react
+      else
+        msg.react('❌').catch()
 
-    message.channel.stopTyping()
-  })
-  .catch( err => {console.log(err); setTimeout(() => {throw err})} )
+      message.channel.stopTyping()
+    })
+    .catch(err => bot.modules.reportError(err, 'Search Send'))
 }
