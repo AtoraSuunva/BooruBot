85b9cfbc58f372b9271a33a9f780072ae2542df0
AtlasTheBot <AtlasTheBot@users.noreply.github.com>
2017-12-08 23:07:43 -0500

    Use array destructuring in botban; Add usage help

--- modules/core/botban.js
+++ modules/core/botban.js
@@ -4,35 +4,35 @@ module.exports.config = {
   name: 'botban',
   invokers: ['botban', 'don\'t talk to me or my bot ever again', 'unbotban'],
   help: 'botbans people',
-  expandedHelp: 'Botbans people. `botban [@mention | ID]\nalso why are you searching help for this only the owner can use this`',
+  expandedHelp: 'Botbans people. also why are you searching help for this only the owner can use this`',
+  usage: ['Botban', 'botban [@mention|id] [reason]', 'Unbotban', 'unbotban [@mention|id]'],
   invisible: true
 }
 
 module.exports.events = {}
 module.exports.events.message = (bot, message) => {
   let config = bot.modules.config
-  let args = bot.modules.shlex(message.content.replace('don\'t talk to me or my bot ever again', 'botban'))
-
+  let [cmd, userToBotban, ...reason] = bot.modules.shlex(message.content.replace('don\'t talk to me or my bot ever again', 'botban'))
+  reason = reason.join(' ')
 
   if (message.author.id !== config.owner.id) return message.channel.send('no.')
 
-  if (args[1] === undefined) return message.author.send(config.botbans.map(b=>`**${b.name}**#${b.discriminator} (${b.id}): ${b.reason}`).join('\n')||'No botbans.')
+  if (userToBotban === undefined) return message.author.send(config.botbans.map(b=>`**${b.name}**#${b.discriminator} (${b.id}): ${b.reason}`).join('\n')||'No botbans.')
 
-  let user = (message.mentions.users.first()) ? message.mentions.users.first().id : args[1]
+  let user = (message.mentions.users.first()) ? message.mentions.users.first().id : userToBotban
 
   if (user === config.owner.id) return message.channel.send(`That's a bad idea...`)
   if (user === bot.user.id) return message.channel.send(`What would that even accomplish`)
 
   //unbotban id/name/discriminator thething
 
-
   bot.fetchUser(user).then(user => {
     let msg = ''
-    if (args[0] === 'botban') {
-      config.botbans.push({name: user.username, discriminator: user.discriminator, id: user.id, reason: args.splice(2).join(' ')})
+    if (cmd === 'botban') {
+      config.botbans.push({name: user.username, discriminator: user.discriminator, id: user.id, reason})
       msg = `Botbanned ${user.username}.`
     } else {
-      config.botbans = config.botbans.filter(b=>b[args[1]] !== args[2])
+      config.botbans = config.botbans.filter(b => b.id === user.id)
       msg = `Unbotbanned ${user.username}.`
     }
 
