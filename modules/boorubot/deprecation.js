727b6e05a813d2f24930cf58113caab4c765213a
AtlasTheBot <AtlasTheBot@users.noreply.github.com>
2018-01-15 20:35:12 -0500

    Fix deprecation.js and modify deprecation messages a bit

--- modules/boorubot/deprecation.js
+++ modules/boorubot/deprecation.js
@@ -2,39 +2,37 @@
 
 module.exports.config = {
   name: 'deprecation',
-  invokers: ['='],
   help: 'reee old boorubot',
   expandedHelp: 'what are you doing searching help for this stop',
   invisible: true
 }
 
 const deprecations = [
-  {oldCommand: '=help', newCommand: 'b!help'},
-  {oldCommand: '=setting', newCommand: 'b!setting'},
+  {oldCommand: '=settings', newCommand: 'b!setting'},
   {oldCommand: '=blacklist', newCommand: 'b!blacklist'},
   {oldCommand: '=whitelist', newCommand: 'b!whitelist'},
-  {oldCommand: '=add', newCommand: 'No command anymore...'},
-  {oldCommand: '=remove', newCommand: 'No command anymore...'},
-  {oldCommand: '=avy', newCommand: 'b!avy'},
+  {oldCommand: '=add', newCommand: 'Blacklist is now based on the "Manage Messages" perm'},
+  {oldCommand: '=remove', newCommand: 'Blacklist is now based on the "Manage Messages" perm'},
+  {oldCommand: '=avy', newCommand: 'No command anymore.'},
   {oldCommand: '=invite', newCommand: 'b!invite'},
-  {oldCommand: '=rand', newCommand: 'b!s rand'},
-  {oldCommand: '=eval', newCommand: 'atlas pls'}
+  {oldCommand: '=rand', newCommand: 'b!s rand (or) b!r'},
 ]
 
 const booru = require('booru')
 for (let site of Object.keys(booru.sites)) {
   deprecations.push({oldCommand: `=${site}`, newCommand: `b!s ${site}`})
-  for (let alias of booru.sites[site].aliases) deprecations.push({oldCommand: `=${alias}`, newCommand: `b!s ${alias}`})
+  for (let alias of booru.sites[site].aliases) deprecations.push({oldCommand: `=${alias}`, newCommand: `b!s ${alias} (or) b!${alias}`})
 }
 
 module.exports.events = {}
-module.exports.events.message = (bot, message) => {
+module.exports.events.everyMessage = (bot, message) => {
   const settingsId = (message.guild !== null) ? message.guild.id : message.channel.id
   const settings = bot.modules.settings.get(settingsId)
 
   if (!settings.options.deprecationWarning) return;
 
   for (let deprecation of deprecations) {
-    if (message.content.startsWith(deprecation.oldCommand)) message.channel.send(`\`${deprecation.oldCommand}\` is deprecated, use \`${deprecation.newCommand}\` instead.\n*Use \`b!set deprecationWarning false\` to disable this message, \`b!help ${deprecation.newCommand.replace('b!','')}\` for help.*`)
+    if (message.content.startsWith(deprecation.oldCommand))
+      message.channel.send(`\`${deprecation.oldCommand}\` is deprecated, use \`${deprecation.newCommand}\` instead.\n*Use \`b!set deprecationWarning false\` to disable this message, \`b!help ${deprecation.newCommand.replace('b!','')}\` for help.*`)
   }
 }
